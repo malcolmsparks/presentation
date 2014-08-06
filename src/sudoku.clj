eec4df9d878f2b09d3f31d6324df2c4abde893b6
Malcolm Sparks <malcolm@juxt.pro>
2014-08-06 15:16:59 +0100

    Tidy up

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -7,21 +7,7 @@
    [hiccup.core :refer (html)]
    [clojure.pprint :refer (pprint)]))
 
-(let [grid
-      [[8 0 0 0 0 0 0 0 0]
-       [0 0 3 6 0 0 0 0 0]
-       [0 7 0 0 9 0 2 0 0]
-       [0 5 0 0 0 7 0 0 0]
-       [0 0 0 0 4 5 7 0 0]
-       [0 0 0 1 0 0 0 3 0]
-       [0 0 1 0 0 0 0 6 8]
-       [0 0 8 5 0 0 0 1 0]
-       [0 9 0 0 0 0 4 0 0]]
-      hints (apply concat grid)
-      ]
-  hints
-  )
+(def example '(8 0 0 0 0 0 0 0 0 0 0 3 6 0 0 0 0 0 0 7 0 0 9 0 2 0 0 0 5 0 0 0 7 0 0 0 0 0 0 0 4 5 7 0 0 0 0 0 1 0 0 0 3 0 0 0 1 0 0 0 0 6 8 0 0 8 5 0 0 0 1 0 0 9 0 0 0 0 4 0 0))
 
 (defn solve [hints]
-  (repeatedly 81 #(rand-nth [1 2 3 4 5 6 7 8 9]))
-  )
+  (repeatedly 81 #(rand-nth [0 1 2 3 4 5 6 7 8 9])))
